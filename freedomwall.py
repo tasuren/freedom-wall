2561c84029a1c526e1a667b8a3b1874204ccd893
tasuren <45121209+tasuren@users.noreply.github.com>
2021-02-21 22:03:30 +0900

    初回アップロード

---
+++ freedomwall.py
@@ -0,0 +1,186 @@
+r""" 
+  ______                 _                 
+ |  ____|               | |                
+ | |__ _ __ ___  ___  __| | ___  _ __ ___  
+ |  __| '__/ _ \/ _ \/ _` |/ _ \| '_ ` _ \ 
+ | |  | | |  __/  __/ (_| | (_) | | | | | |
+ |_|__|_|_ \___|\___|\__,_|\___/|_| |_| |_|
+ |  __ \(_)                                
+ | |  | |___   _____                       
+ | |  | | \ \ / / _ \      by tasuren
+ | |__| | |\ V /  __/                      
+ |_____/|_| \_/ \___|                      
+
+"""
+
+from util.win32 import GetActiveWindow,GetWindowRect,setClickthrough,GetWindow
+from util.tasktray import TaskTray
+from util.player import TkPlayer
+
+from tkinter import Tk,ttk,Canvas,BOTH,NW
+from threading import Thread
+from json import load,dump
+
+
+
+class Wallcord():
+    def __init__(self,root):
+        self.root = root
+
+        with open("data.json","r") as f:
+            self.data = load(f)
+
+        self.targets = list(self.data["windows"].keys())
+
+        self.window_title = "tk"
+        self.window_show = True
+        self.video = TkPlayer("")
+        self.now_window = ""
+        self.onoff = True
+        self.now = ""
+        self.q = []
+
+        self.create_window()
+
+        self.video_player()
+        self.discord_and_open()
+
+    # ウィンドウセッティング。
+    def create_window(self):
+        # ウィンドウタイトルを設定する。
+        self.root.title(self.window_title)
+        # ウィンドウタイトルバーを非表示にする。
+        # self.debugがTrueの場合わかるように非表示にしない。
+        self.root.overrideredirect(1)
+        # 最小ウィンドウサイズを設定する。
+        self.root.minsize(width=10,height=10)
+        self.width,self.height = 10,10
+
+        # 白色は透明化にする。
+        self.root.wm_attributes("-transparentcolor","white")
+        # 全体的に薄く表示する。
+        self.root.attributes("-alpha",0.1)
+        # 動画再生部分の表示。
+        self.canvas = Canvas(self.root,width=5,height=5)
+        self.canvas.pack(expand=True,fill=BOTH)
+
+        # タイトルバー非表示にする。
+        root.overrideredirect(1)
+
+        # クリック無効にする。
+        self.root.update()
+        setClickthrough(int(root.frame(),16))
+
+        # アイコンを設定する。
+        self.root.iconbitmap(default="icon.ico")
+
+        # ウィンドウを非表示状態にする。
+        self.root.withdraw()
+        self.root.attributes("-topmost",False)
+
+        # 終了時は終了するか聞く。
+        root.protocol("WM_DELETE_WINDOW",lambda: root.destroy() if messagebox.askokcancel("FreedomWall","壁紙の設定を終了しますか？") else print())
+
+
+    # データリロード。
+    def reload(self):
+        with open("data.json","r") as f:
+            self.data = load(f)
+        self.targets = list(self.data["windows"].keys())
+
+    # 壁紙再生と設定画面実行のループ。
+    def video_player(self):
+
+        # 壁紙再生。
+        # 未設定じゃない時のみ実行する。
+        if self.now != "" or self.onoff:
+            # 他のウィンドウになったらそのウィンドウの壁紙に変更
+            if self.now != self.video.path:
+                self.video = TkPlayer(self.now)
+
+            # ターゲットのウィンドウが表示されている時だけ実行する。
+            if self.window_show:
+                # 描画はutil/player.pyに主要コードがある。
+                self.image = self.video.get_frame(self.height,self.width)
+                if self.image:
+                    self.canvas.create_image(0,0,image=self.image,anchor=NW)
+
+
+        # 設定などのタスクトレイから実行するやつをメインスレッドで実行するためのもの。
+        # Tkinterのメソッドはメインスレッドからじゃないと実行できないためキューに追加してここで実行する。
+        if self.q:
+            for q in self.q:
+                q()
+                self.q.pop(0)
+
+        self.root.after(int(1/self.video.fps*100),self.video_player)
+
+    # Discord開くときだけウィンドウを表示するためのループ。
+    def discord_and_open(self):
+        # ONのときだけ
+        if self.onoff:
+
+            # --handle,window_name = GetActiveWindow()
+            # WinAPIを使用してアクティブのウィンドウを取得する。
+
+            # -- if "Discord" in window_name or self.window_title == window_name:
+            # Discordがアクティブか確認する。
+            # このときtkが反応することがあるから、それも確認対象にする。
+
+            # -- if self.now != self.data["windows"][target]:
+            # 今のウィンドウが変わったらその壁紙を設定する。
+
+            # -- if ? self.window_show and target in window_name:
+            # ウィンドウ表示非表示動作をします。
+            # self.whindow_showで無駄な動作をなくす。
+            # self.root.attributes("-alpha",int)でウィンドウを薄くする。
+            # なくさないとウィンドウ移動がしずらくなるなど支障が出る。
+            # attributes("-topmost")で常前に行くようにする
+
+            # -- if self.window_title != window_name:
+            # Discordがアクティブの状態のみ実行するものです。
+            # WinAPIで座標を取得して調整。
+
+            handle,window_name = GetActiveWindow()
+            if handle != 0:
+                # 開発中の誤検出用にSubllimeTextは無視する。
+                # 対象が空の場合も無視する。
+                if not "Sublime Text" in window_name and self.targets:
+                    for target in self.targets:
+                        if target in window_name or self.window_title == window_name:
+                            path,alpha = self.data["windows"][target]
+                            if self.now != path:
+                                self.now = path
+                                self.now_window = target
+
+                            if not self.window_show and target in window_name:
+                                self.root.attributes("-alpha",alpha)
+                                self.root.deiconify()
+                                self.window_show = True
+                                self.root.attributes("-topmost",True)
+
+                            if self.window_title != window_name:
+                                x0,y0,x1,y1 = GetWindowRect(handle)
+                                sa = 0
+                                self.width,self.height = x1-x0,y1-y0
+                                self.root.geometry(f"{self.width}x{self.height}+{x0-sa}+{y0-sa}")
+
+                        elif self.window_show and target == self.now_window:
+                            self.root.withdraw()
+                            self.window_show = False
+                            self.root.attributes("-topmost",False)
+
+                    # ターゲットがない際は実行しないようにする。
+                    if not self.window_show:
+                        self.now = ""
+
+        self.root.after(10,self.discord_and_open)
+
+
+root = Tk()
+wallcord = Wallcord(root)
+tasktray = TaskTray(wallcord)
+    
+
+Thread(target=lambda:tasktray.run()).start()
+root.mainloop()
---
+++ util/tasktray.py
@@ -0,0 +1,123 @@
+# FreedomWall - タスクトレイモジュール
+
+from util.win32 import GetWindow
+
+from tkinter import simpledialog,filedialog,messagebox
+from infi.systray import SysTrayIcon
+
+from json import load,dump
+from PIL import Image
+
+
+credit_text = """# Freedom Wall 1.1.0
+開発言語：Python
+開発者　：tasuren
+
+Copyright (C) 2021 by tasuren
+
+# 使用ライブラリ
+## Pillow
+Copyright (C) 1997-2011 by Secret Labs AB
+Copyright (C) 1995-2011 by Fredrik Lundh
+## OpenCV
+Copyright (C) 2000-2020, Intel Corporation, all rights reserved.
+Copyright (C) 2009-2011, Willow Garage Inc., all rights reserved.
+Copyright (C) 2009-2016, NVIDIA Corporation, all rights reserved.
+Copyright (C) 2010-2013, Advanced Micro Devices, Inc., all rights reserved.
+Copyright (C) 2015-2016, OpenCV Foundation, all rights reserved.
+Copyright (C) 2015-2016, Itseez Inc., all rights reserved.
+Copyright (C) 2019-2020, Xperience AI, all rights reserved.
+## Threading
+Copyright (c) 2019 Loreto Parisi
+## infi.systray
+Copyright (c) 2017 INFINIDAT
+## ライセンス
+上記のライブラリのライセンスはFreedomWallフォルダのLICENSEフォルダにテキストファイルとして記載されています。"""
+
+
+class TaskTray():
+	def __init__(self,wallcord):
+		self.window = wallcord
+		self.root = self.window.root
+
+		with open("data.json","r") as f:
+			self.data = load(f)
+
+		# メインスレッドじゃないとTkinterメソッドを実行できない。
+		# だからメインスレッドのFreedomWallクラスのself.qに実行したいのを追加する。
+		# そしてメインスレッドから実行する。
+		# だから lambda がある。
+		self.icon = SysTrayIcon(
+			"icon.ico",
+			"FreedomWall",
+			(
+				("FreedomWall",None,lambda sysTrayIcon: self.window.q.append(self.credit)),
+				("Set",None,lambda sysTrayIcon: self.window.q.append(self.setting)),
+				("Del",None,lambda sysTrayIcon: self.window.q.append(self.delete)),
+				("List",None,lambda sysTrayIcon: self.window.q.append(self.setting_list))
+			),
+			on_quit=lambda sysTrayIcon: self.window.q.append(self.exit)
+		)
+
+	# 壁紙の設定。
+	def setting(self):
+		title = simpledialog.askstring("FreedomWall","設定したいウィンドウのタイトルにある文字を入力してください。")
+		if not title:
+			return
+		alpha = simpledialog.askstring("FreedomWall","壁紙の透明度を入力してください。\nデフォルトは0.2です。\n元の背景が白の場合は0.3あたりの数値が良いです。\n元の背景が黒の場合は0.1あたりの数値が良いです。")
+		if not alpha:
+			alpha = 0.2
+		try:
+			alpha = float(alpha)
+		except:
+			messagebox.showwarning("FreedomWall","0.1 ~ 1 の間を設定してください。")
+			return
+		file_name = filedialog.askopenfilename(filetypes=[("動画ファイル","*.mp4"),("画像ファイル","*.png;*.jpg")])
+		if not file_name:
+			return
+
+		if not GetWindow(title,"bubun"):
+			messagebox.showwarning("FreedomWall",f"{title}があるタイトルのウィンドウが見つかりませんでした。")
+			return
+
+		self.data["windows"][title] = [file_name,alpha]
+		with open("data.json","w") as f:
+			dump(self.data,f,indent=4)
+		self.window.reload()
+		self.window.now = ""
+		messagebox.showinfo("FreedomWall","設定しました。")
+
+	# 壁紙の削除。
+	def delete(self):
+		title = simpledialog.askstring("FreedomWall","削除したい設定名を入力してください。")
+		if not title:
+			return
+		if not title in self.data["windows"]:
+			messagebox.showwarning("FreedomWall","その設定が見つかりませんでした。")
+			return
+		del self.data["windows"][title]
+		with open("data.json","w") as f:
+			dump(self.data,f,indent=4)
+		self.window.reload()
+		self.window.now = ""
+		messagebox.showinfo("FreedomWall","その設定を削除しました。")
+
+	# 壁紙一覧。
+	def setting_list(self):
+		desc = ", ".join(data for data in self.data["windows"].keys())
+		messagebox.showinfo("FreedomWall",desc)
+
+	# クレジット。
+	def credit(self):
+		messagebox.showinfo("FreedomWall",credit_text)
+
+	# 終了。
+	def exit(self):
+		self.root.quit()
+		self.window.video = None
+		self.icon.shutdown()
+		exit()
+
+	# 実行。
+	def run(self):
+		self.icon.start()
