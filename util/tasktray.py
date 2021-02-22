f0bd9171691b833124c5f3dc7d441433fbaa60fe
tasuren <45121209+tasuren@users.noreply.github.com>
2021-02-22 19:58:17 +0900

    起動時にメッセージを追加

--- util/tasktray.py
+++ util/tasktray.py
@@ -36,88 +36,89 @@ Copyright (c) 2017 INFINIDAT
 
 
 class TaskTray():
-	def __init__(self,wallcord):
-		self.window = wallcord
-		self.root = self.window.root
+    def __init__(self,wallcord):
+        self.window = wallcord
+        self.root = self.window.root
 
-		with open("data.json","r") as f:
-			self.data = load(f)
+        with open("data.json","r") as f:
+            self.data = load(f)
 
-		# メインスレッドじゃないとTkinterメソッドを実行できない。
-		# だからメインスレッドのFreedomWallクラスのself.qに実行したいのを追加する。
-		# そしてメインスレッドから実行する。
-		# だから lambda がある。
-		self.icon = SysTrayIcon(
-			"icon.ico",
-			"FreedomWall",
-			(
-				("FreedomWall",None,lambda sysTrayIcon: self.window.q.append(self.credit)),
-				("Set",None,lambda sysTrayIcon: self.window.q.append(self.setting)),
-				("Del",None,lambda sysTrayIcon: self.window.q.append(self.delete)),
-				("List",None,lambda sysTrayIcon: self.window.q.append(self.setting_list))
-			),
-			on_quit=lambda sysTrayIcon: self.window.q.append(self.exit)
-		)
+        # メインスレッドじゃないとTkinterメソッドを実行できない。
+        # だからメインスレッドのFreedomWallクラスのself.qに実行したいのを追加する。
+        # そしてメインスレッドから実行する。
+        # だから lambda がある。
+        self.icon = SysTrayIcon(
+            "icon.ico",
+            "FreedomWall",
+            (
+                ("FreedomWall",None,lambda sysTrayIcon: self.window.q.append(self.credit)),
+                ("Set",None,lambda sysTrayIcon: self.window.q.append(self.setting)),
+                ("Del",None,lambda sysTrayIcon: self.window.q.append(self.delete)),
+                ("List",None,lambda sysTrayIcon: self.window.q.append(self.setting_list))
+            ),
+            on_quit=lambda sysTrayIcon: self.window.q.append(self.exit)
+        )
 
-	# 壁紙の設定。
-	def setting(self):
-		title = simpledialog.askstring("FreedomWall","設定したいウィンドウのタイトルにある文字を入力してください。")
-		if not title:
-			return
-		alpha = simpledialog.askstring("FreedomWall","壁紙の透明度を入力してください。\nデフォルトは0.2です。\n元の背景が白の場合は0.3あたりの数値が良いです。\n元の背景が黒の場合は0.1あたりの数値が良いです。")
-		if not alpha:
-			alpha = 0.2
-		try:
-			alpha = float(alpha)
-		except:
-			messagebox.showwarning("FreedomWall","0.1 ~ 1 の間を設定してください。")
-			return
-		file_name = filedialog.askopenfilename(filetypes=[("動画ファイル","*.mp4"),("画像ファイル","*.png;*.jpg")])
-		if not file_name:
-			return
+    # 壁紙の設定。
+    def setting(self):
+        title = simpledialog.askstring("FreedomWall","設定したいウィンドウのタイトルにある文字を入力してください。")
+        if not title:
+            return
+        alpha = simpledialog.askstring("FreedomWall","壁紙の透明度を入力してください。\nデフォルトは0.2です。\n元の背景が白の場合は0.3あたりの数値が良いです。\n元の背景が黒の場合は0.1あたりの数値が良いです。")
+        if not alpha:
+            alpha = 0.2
+        try:
+            alpha = float(alpha)
+        except:
+            messagebox.showwarning("FreedomWall","0.1 ~ 1 の間を設定してください。")
+            return
+        file_name = filedialog.askopenfilename(filetypes=[("動画ファイル","*.mp4"),("画像ファイル","*.png;*.jpg")])
+        if not file_name:
+            return
 
-		if not GetWindow(title,"bubun"):
-			messagebox.showwarning("FreedomWall",f"{title}があるタイトルのウィンドウが見つかりませんでした。")
-			return
+        if not GetWindow(title,"bubun"):
+            messagebox.showwarning("FreedomWall",f"{title}があるタイトルのウィンドウが見つかりませんでした。")
+            return
 
-		self.data["windows"][title] = [file_name,alpha]
-		with open("data.json","w") as f:
-			dump(self.data,f,indent=4)
-		self.window.reload()
-		self.window.now = ""
-		messagebox.showinfo("FreedomWall","設定しました。")
+        self.data["windows"][title] = [file_name,alpha]
+        with open("data.json","w") as f:
+            dump(self.data,f,indent=4)
+        self.window.reload()
+        self.window.now = ""
+        messagebox.showinfo("FreedomWall","設定しました。")
 
-	# 壁紙の削除。
-	def delete(self):
-		title = simpledialog.askstring("FreedomWall","削除したい設定名を入力してください。")
-		if not title:
-			return
-		if not title in self.data["windows"]:
-			messagebox.showwarning("FreedomWall","その設定が見つかりませんでした。")
-			return
-		del self.data["windows"][title]
-		with open("data.json","w") as f:
-			dump(self.data,f,indent=4)
-		self.window.reload()
-		self.window.now = ""
-		messagebox.showinfo("FreedomWall","その設定を削除しました。")
+    # 壁紙の削除。
+    def delete(self):
+        title = simpledialog.askstring("FreedomWall","削除したい設定名を入力してください。")
+        if not title:
+            return
+        if not title in self.data["windows"]:
+            messagebox.showwarning("FreedomWall","その設定が見つかりませんでした。")
+            return
+        del self.data["windows"][title]
+        with open("data.json","w") as f:
+            dump(self.data,f,indent=4)
+        self.window.reload()
+        self.window.now = ""
+        messagebox.showinfo("FreedomWall","その設定を削除しました。")
 
-	# 壁紙一覧。
-	def setting_list(self):
-		desc = ", ".join(data for data in self.data["windows"].keys())
-		messagebox.showinfo("FreedomWall",desc)
+    # 壁紙一覧。
+    def setting_list(self):
+        desc = ", ".join(data for data in self.data["windows"].keys())
+        messagebox.showinfo("FreedomWall",desc)
 
-	# クレジット。
-	def credit(self):
-		messagebox.showinfo("FreedomWall",credit_text)
+    # クレジット。
+    def credit(self):
+        messagebox.showinfo("FreedomWall",credit_text)
 
-	# 終了。
-	def exit(self):
-		self.root.quit()
-		self.window.video = None
-		self.icon.shutdown()
-		exit()
+    # 終了。
+    def exit(self):
+        self.root.quit()
+        self.window.video = None
+        self.icon.shutdown()
+        exit()
 
-	# 実行。
-	def run(self):
-		self.icon.start()
+    # 実行。
+    def run(self):
+        messagebox.showinfo("FreedomWall","起動しました。\n設定する場合は右下のタスクトレイから月のマークを右クリックして使用してください。")
+        self.icon.start()
